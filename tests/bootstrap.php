250947531fc6eae476dee93a5aba6dfbdb5896e5
Melinda S <roseofgold@gmail.com>
2019-05-04 14:24:24 -0700

    list classes for use in testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+include __DIR__ . '/../classes/Collection.php';
+include __DIR__ . '/../classes/ListingBasic.php';
+include __DIR__ . '/../classes/ListingPremium.php';
+include __DIR__ . '/../classes/ListingInactive.php';
+include __DIR__ . '/../classes/ListingFeatured.php';
